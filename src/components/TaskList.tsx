d54842c47e7dededa7ee76aa30a9124ea1296159
Julia Alkhimova <devalchem@gmail.com>
2026-02-24 12:14:53 +0100

    feat: add and manage states for TaskList component

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -10,28 +10,59 @@ type TaskListProps = {
 };
 
 export default function TaskList({
-    loading = false,
-    tasks,
+  loading = false,
+  tasks,
+  onPinTask,
+  onArchiveTask,
+}: TaskListProps) {
+  const events = {
     onPinTask,
     onArchiveTask,
-}: TaskListProps) {
-    const events = {
-        onPinTask,
-        onArchiveTask
-    };
+  };
 
-    if(loading){
-        return <div className="list-items">loading</div>
-    }
-    if(tasks.length === 0){
-        return <div className="list-items">empty</div>
-    }
+  const LoadingRow = (
+    <div className="loading-item">
+      <span className="glow-checkbox" />
+      <span className="glow-text">
+        <span>Loading</span> <span>cool</span> <span>state</span>
+      </span>
+    </div>
+  );
 
+  if (loading) {
     return (
-        <div className="list-item">
-            {tasks.map(task=>(
-                <Task key={task.id} task={task} {...events}/>
-            ))}
+      <div className="list-items" data-testid="loading" key={"loading"}>
+        {LoadingRow}
+        {LoadingRow}
+        {LoadingRow}
+        {LoadingRow}
+        {LoadingRow}
+        {LoadingRow}
+      </div>
+    );
+  }
+  if (tasks.length === 0) {
+    return (
+      <div className="list-items" data-testid="empty" key={"empty"}>
+        <div className="wrapper-message">
+          <span className="icon-check" />
+          <p className="title-message">You have no tasks</p>
+          <p className="subtitle-message">Sit back and relax</p>
         </div>
-    )
+      </div>
+    );
+  }
+  const taskInOrder = [
+    ...tasks.filter((t)=> t.state === "TASK_PINNED"),
+    ...tasks.filter((t)=> t.state === "TASK_INBOX"),
+    ...tasks.filter((t)=> t.state === "TASK_ARCHIVED"),
+   
+  ]
+  return (
+    <div className="list-item">
+      {taskInOrder.map((task) => (
+        <Task key={task.id} task={task} {...events} />
+      ))}
+    </div>
+  );
 }
