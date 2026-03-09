1ed7c1b4b7c258e8b22ee7dc1d99e32773ba813f
Julia Alkhimova <devalchem@gmail.com>
2026-03-09 10:18:42 +0100

    feat(inboxScreen): add play function for default storie to test correct behaviour

--- src/components/InboxScreen.stories.tsx
+++ src/components/InboxScreen.stories.tsx
@@ -1,6 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react-vite';
+import { waitFor, waitForElementToBeRemoved } from 'storybook/test';
 import { http, HttpResponse } from 'msw';
-
 import { MockedState } from './TaskList.stories';
 import { Provider } from 'react-redux';
 
@@ -19,27 +19,41 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
- parameters: {
+  parameters: {
    msw: {
      handlers: [
-       http.get('https://jsonplaceholder.typicode.com/todos?userId=1', () => {
+      http.get('https://jsonplaceholder.typicode.com/todos?userId=1', () => {
          return HttpResponse.json(MockedState.tasks);
        }),
      ],
    },
  },
+
+
+play: async ({ canvas, userEvent }) => {
+   // Waits for the component to transition from the loading state
+   await waitForElementToBeRemoved(await canvas.findByTestId('loading'));
+   // Waits for the component to be updated based on the store
+   await waitFor(async () => {
+     // Simulates pinning the first task
+     await userEvent.click(canvas.getByLabelText('pinTask-1'));
+     // Simulates pinning the third task
+     await userEvent.click(canvas.getByLabelText('pinTask-3'));
+   });
+ },
 };
 
 export const Error: Story = {
- parameters: {
+  parameters: {
    msw: {
      handlers: [
-       http.get('https://jsonplaceholder.typicode.com/todos?userId=1', () => {
+      http.get('https://jsonplaceholder.typicode.com/todos?userId=1', () => {
          return new HttpResponse(null, {
-           status: 403,
+          status: 403,
          });
-       }),
+       })
      ],
    },
  },
 };
+
