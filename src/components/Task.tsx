1bd5f9294fd7b4b312860dcce0679089bdf685e2
Julia Alkhimova <devalchem@gmail.com>
2026-03-10 08:21:31 +0100

    styles(task): change task background to lightblue

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -36,7 +36,7 @@ export default function Task({
           name="title"
           id={`title-${id}`}
           placeholder="Input title"
-          style={{ backgroundColor: 'lightpink' }}
+          style={{ backgroundColor: 'lightblue' }}
         />
       </label>
 
